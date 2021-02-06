622e6fcb0c9ae01d52af6528782665fabe13c283
Abhishek Sardagi <sonuabhsardagi@gmail.com>
2021-02-06 08:33:31 +0530

    index & product

---
+++ features.js
@@ -0,0 +1,18 @@
+// var parent = document.querySelector("div");
+// var btn = parent.
+
+// var showAlert = function() {
+//     alert("This feature is under construction");
+// }
+
+// btn.addEventListener("click", showAlert);
+
+// var parent = document.getElementsByClassName("box")
+// var btn = document.querySelector("img")
+
+// var showAlert = function() {
+//     alert("This feature is under construction");
+
+// }
+
+// btn.addEventListener("click", showAlert);
